35bbb8ea291fc056ada08363dd21926940d8865a
Juli <yuliya.surakshina@phystech.edu>
2016-04-25 14:45:31 +0300

    Initial Commit

---
+++ Tutu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tutu
+//
+//  Created by Juli on 25.04.16.
+//  Copyright © 2016 Juli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tutu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tutu
+//
+//  Created by Juli on 25.04.16.
+//  Copyright © 2016 Juli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
